ae8fbcb86e06f255ea716969b92a06d02506b301
Paul Greetham <pgreetham@protonmail.com>
2023-12-31 16:57:18 +0100

    home page developed

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -9,5 +9,20 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  name = 'Paul Greetham';
+  lettersWithDelay: Array<{ letter: string, delay: string } | 'BR'> = [];
 
+  constructor() {
+    let delay = 0;
+    const increment = 0.15;
+
+    for (const char of this.name) {
+      if (char === ' ') {
+        this.lettersWithDelay.push('BR');
+        continue;
+      }
+      this.lettersWithDelay.push({ letter: char, delay: `${delay}s` });
+      delay += increment;
+    }
+  }
 }
