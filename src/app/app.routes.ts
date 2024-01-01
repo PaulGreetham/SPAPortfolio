c4bfa3a1e4f1cbdea3f57878f0f35e3a4ac751ab
Paul Greetham <pgreetham@protonmail.com>
2024-01-01 10:02:16 +0100

    home page reload created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, pathMatch: 'full' },
   { path: 'about', component: AboutComponent },
   { path: 'tech', component: TechComponent },
   { path: 'projects', component: ProjectsComponent },
