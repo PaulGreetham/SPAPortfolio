9988f487a1da5a9ea7f8fb4dbad69eacc3640a57
Paul Greetham <pgreetham@protonmail.com>
2024-01-04 14:10:59 +0100

    professional section created and developed

--- src/app/Shared-Components/accordion/professional/professional.component.ts
+++ src/app/Shared-Components/accordion/professional/professional.component.ts
@@ -2,6 +2,18 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ModalComponent } from "../../modal/modal.component";
 import { CommonModule } from '@angular/common';
 
+export interface JobReference {
+  logoUrl: string;
+  companyName: string;
+  companyWebsite: string;
+  jobTitle: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  info: string;
+  dutiesAndTools: string[];
+}
+
 @Component({
     selector: 'app-professional',
     standalone: true,
@@ -12,6 +24,18 @@ import { CommonModule } from '@angular/common';
 export class ProfessionalComponent {
   @Input() item: any;
   @Output() modalToggle = new EventEmitter<boolean>();
+
+  jobReference: JobReference = {
+    logoUrl: '/assets/logos/RiskChallenger.jpeg',
+    companyName: 'RiskChallenger',
+    companyWebsite: 'https://www.riskchallenger.nl/',
+    jobTitle: 'Junior Full-Stack Software Developer',
+    startDate: 'April 2023',
+    endDate: 'Present',
+    location: 'Amersfoort, The Netherlands',
+    info: 'A brief description of the role and responsibilities.',
+    dutiesAndTools: ['Duty 1', 'Duty 2', 'Tool A', 'Tool B'],
+  };
 
   openModalItem: string | null = null;
   showModal = false;
