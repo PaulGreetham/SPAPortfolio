87d02933e6cf4194b93f21ffef2753ad764dc3d0
Paul Greetham <pgreetham@protonmail.com>
2024-05-05 19:57:43 +0200

    project grid system created for 6 projects

--- src/app/Pages/projects/projects.component.ts
+++ src/app/Pages/projects/projects.component.ts
@@ -15,5 +15,7 @@ export class ProjectsComponent {
     { id: 'card-2', title: 'Project 2', description: 'Description 2', animationClass: 'animate-delay-2' },
     { id: 'card-3', title: 'Project 3', description: 'Description 3', animationClass: 'animate-delay-3' },
     { id: 'card-4', title: 'Project 4', description: 'Description 4', animationClass: 'animate-delay-4' },
+    { id: 'card-5', title: 'Project 5', description: 'Description 5', animationClass: 'animate-delay-5' },
+    { id: 'card-6', title: 'Project 6', description: 'Description 6', animationClass: 'animate-delay-6' },
   ];
 }
