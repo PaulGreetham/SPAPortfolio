8028db1e23cf10547b62442ab4cb502cf78ab75a
Paul Greetham <pgreetham@protonmail.com>
2024-06-06 10:04:02 +0200

    bootcamp layout ammended

--- src/app/Shared-Components/accordion/bootcamps/bootcamps.component.ts
+++ src/app/Shared-Components/accordion/bootcamps/bootcamps.component.ts
@@ -15,10 +15,10 @@ export class BootcampsComponent {
       name: '</SALT>',
       url: 'https://www.salt.dev',
       startDate: 'January 2023',
-      endDate: 'March 2023',
+      endDate: 'April 2023',
       location: 'Amsterdam, Netherlands',
-      info: '</SALT> was an intensive full-time 13-week full-satck software developement bootcamp that focussed on Agile group programming. Subjects and tools used include:',
-      contentList: this.splitContent('HTML * CSS & SCSS * JavaScript * TypeScript * React * Redux * Node.js * MongoDB * Express * RESTful APIs * Jest * Cypress * TDD * e2e Testing * CI/CD * Babel * GitHub * VS Code')
+      info: '</SALT> (The School of Applied Technology) was an intensive full-time 13-week full-satck software developement bootcamp that focussed on Agile group programming and continuous personal performance testing. Subjects and tools used include:',
+      contentList: this.splitContent('React * Redux * TypeScript * Node.js * HTML * CSS & SCSS * JavaScript * MongoDB * Express * RESTful APIs * Jest * Cypress * TDD * e2e Testing * CI/CD * Babel * GitHub * VS Code')
     },
     {
       logo: '/assets/logos/LeWagon.png',
@@ -27,8 +27,8 @@ export class BootcampsComponent {
       startDate: 'July 2022',
       endDate: 'September 2022',
       location: 'Amsterdam, Netherlands',
-      info: 'Le Wagon was an intensive full-time 9-week full-stack web development bootcamp that focused on pair programming. Subjects and tools used include:',
-      contentList: this.splitContent('HTML * CSS & SCSS * JavaScript * Ruby * Ruby on Rails * Bootstrp * SQL * RESTful APIs * MVC Principles * VS Code * GitHub * Heroku * AirBnB Clone * Pair Coding * Responsive * Mobile First * AJAX * Figma')
+      info: 'Le Wagon was an intensive full-time 9-week full-stack web development bootcamp that focused on pair programming (the pairs were changed daily to expose students to different ways of working). Subjects and tools used include:',
+      contentList: this.splitContent('Ruby * Ruby on Rails * HTML * CSS & SCSS * JavaScript * Bootstrp * SQL * RESTful APIs * MVC Principles * VS Code * GitHub * Heroku * AirBnB Clone * Pair Coding * Responsive * Mobile First * AJAX * Figma')
     },
   ];
 
