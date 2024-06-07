ff924787396468160c498bedc301af647eddbce9
Paul Greetham <pgreetham@protonmail.com>
2024-06-07 10:48:55 +0200

    added mousedown listenevert to close professional modal

--- src/app/Shared-Components/modal/modal.component.ts
+++ src/app/Shared-Components/modal/modal.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ElementRef, ViewChild, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -8,13 +8,31 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit, OnDestroy {
   @Input() display: boolean = false;
   @Input() title: string = '';
   @Output() closeEvent = new EventEmitter<void>();
 
+  @ViewChild('modalDialog', { static: false }) modalDialog!: ElementRef;
+
+  constructor(private elRef: ElementRef) { }
+
+  ngOnInit() {
+    document.addEventListener('mousedown', this.onModalClick.bind(this));
+  }
+
+  ngOnDestroy() {
+    document.removeEventListener('mousedown', this.onModalClick.bind(this));
+  }
+
   close() {
     this.display = false;
     this.closeEvent.emit();
+  }
+
+  onModalClick(event: MouseEvent) {
+    if (this.display && this.modalDialog && !this.modalDialog.nativeElement.contains(event.target)) {
+      this.close();
+    }
   }
 }
