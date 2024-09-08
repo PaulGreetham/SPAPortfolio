ae66818d96167f0fae11fafd5ffef75eecfa31d6
Paul Greetham <pgreetham@protonmail.com>
2024-09-08 21:42:30 +0200

    stack detials changed

--- src/app/Shared-Components/accordion/stack-tools/stack-tools.component.ts
+++ src/app/Shared-Components/accordion/stack-tools/stack-tools.component.ts
@@ -19,6 +19,7 @@ export class StackToolsComponent {
   @Input() infrequentlyUsedTools: ToolItem[] = [];
 
   frequentlyUsed: ToolItem[] = [
+    { name: 'React', logo: '/assets/logos/React.png', url: 'https://react.dev/' },
     { name: 'Angular', logo: '/assets/logos/Angular.png', url: 'https://angular.dev/' },
     { name: 'TypeScript', logo: '/assets/logos/Typescript.png', url: 'https://www.typescriptlang.org/' },
     { name: 'JavaScript', logo: '/assets/logos/Javascript.png', url: 'https://www.javascript.com/' },
@@ -26,18 +27,17 @@ export class StackToolsComponent {
     { name: 'HTML', logo: '/assets/logos/HTML.png', url: 'https://en.wikipedia.org/wiki/HTML'},
     { name: 'CSS', logo: '/assets/logos/CSS.png', url: 'https://en.wikipedia.org/wiki/CSS' },
     { name: 'SCSS', logo: '/assets/logos/SCSS.png', url: 'https://sass-lang.com/' },
-    { name: 'GitLab', logo: '/assets/logos/Gitlab.png', url: 'https://about.gitlab.com/' },
-    { name: 'Github', logo: '/assets/logos/Github.png', url: 'https://github.com/' },
-    { name: 'Jest', logo: '/assets/logos/Jest.png', url: 'https://jestjs.io/' },
-    { name: 'Cypress', logo: '/assets/logos/Cypress.png', url: 'https://www.cypress.io/' },
-    { name: 'VS Code', logo: '/assets/logos/VSCode.png', url: 'https://code.visualstudio.com/' },
-  ];
-  infrequentlyUsed: ToolItem[] = [
-    { name: 'React', logo: '/assets/logos/React.png', url: 'https://react.dev/' },
-    { name: 'Redux', logo: '/assets/logos/Redux.png', url: 'https://react-redux.js.org/' },
     { name: 'Ruby', logo: '/assets/logos/Ruby.png', url: 'https://www.ruby-lang.org/en/' },
     { name: 'Rails', logo: '/assets/logos/Rails.png', url: 'https://rubyonrails.org/' },
     { name: 'Next', logo: '/assets/logos/Next.png', url: 'https://nextjs.org/' },
+    { name: 'Redux', logo: '/assets/logos/Redux.png', url: 'https://react-redux.js.org/' },
+    ];
+  infrequentlyUsed: ToolItem[] = [
+    { name: 'GitLab', logo: '/assets/logos/Gitlab.png', url: 'https://about.gitlab.com/' },
+    { name: 'Github', logo: '/assets/logos/Github.png', url: 'https://github.com/' },
+    { name: 'VS Code', logo: '/assets/logos/VSCode.png', url: 'https://code.visualstudio.com/' },
+    { name: 'Jest', logo: '/assets/logos/Jest.png', url: 'https://jestjs.io/' },
+    { name: 'Cypress', logo: '/assets/logos/Cypress.png', url: 'https://www.cypress.io/' },
     { name: 'MongoDB', logo: '/assets/logos/MongoDB.png', url: 'https://www.mongodb.com/' },
     { name: 'Docker', logo: '/assets/logos/Docker.png', url: 'https://www.docker.com/' },
     { name: 'SQL', logo: '/assets/logos/SQL.png', url: 'https://en.wikipedia.org/wiki/SQL' },
